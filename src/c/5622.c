7c4920aad183a761ad40c9b49bd7a10e0597aa45
Capella87 <contact@capelladev.com>
2021-01-23 19:14:48 +0900

    Add 3 solutions

---
+++ src/c/5622.c
@@ -0,0 +1,39 @@
+// 백준 5622번 문제 : 다이얼
+// https://www.acmicpc.net/problem/5622
+
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    char input[16];
+    int i;
+    int time = 0;
+
+    scanf("%s", &input);
+    getchar();
+    
+
+    for (i = 0; i < strlen(input); i++)
+    {
+        if (input[i] >= 'A' && input[i] <= 'C')
+            time += 3;
+        else if (input[i] >= 'D' && input[i] <= 'F')
+            time += 4;
+        else if (input[i] >= 'G' && input[i] <= 'I')
+            time += 5;
+        else if (input[i] >= 'J' && input[i] <= 'L')
+            time += 6;
+        else if (input[i] >= 'M' && input[i] <= 'O')
+            time += 7;
+        else if (input[i] >= 'P' && input[i] <= 'S')
+            time += 8;
+        else if (input[i] >= 'T' && input[i] <= 'V')
+            time += 9;
+        else if (input[i] >= 'W' && input[i] <= 'Z')
+            time += 10;
+    }
+    printf("%d\n", time);
+
+    return 0;
+}
