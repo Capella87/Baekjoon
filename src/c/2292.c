9d7871d01bd7d574793d0b0859d482af22f44e33
Capella87 <contact@capelladev.com>
2021-01-24 01:31:58 +0900

    Add 4 new solutions

---
+++ src/c/2292.c
@@ -0,0 +1,21 @@
+// 백준 2292번 문제 : 벌집
+// https://www.acmicpc.net/problem/2292
+
+#include <stdio.h>
+
+int main(void)
+{
+    int input;
+    int result = 1, i = 1;
+
+    scanf("%d", &input);
+
+    while (3 * i * i - 3 * i + 1 < input) // 점화식
+    {
+        result++;
+        i++;
+    }
+    printf("%d\n", result);
+
+    return 0;
+}
---
+++ src/c/2839.c
@@ -0,0 +1,31 @@
+// 백준 2839번 문제 : 설탕 배달
+// https://www.acmicpc.net/problem/2839
+
+#include <stdio.h>
+
+int main(void)
+{
+    int three = 0, five = 0;
+    int n;
+
+    scanf("%d", &n);
+
+    // 일단 5로 나누어 본다.
+    five = n / 5;
+    n %= 5;
+
+    while (five >= 0)
+    {
+        if (n % 3 == 0) // 남은 값들이 3으로 나누어 떨어지는 경우
+        {
+            three = n / 3;
+            n %= 3;
+            break;
+        }
+        five--; // 5로 나누었던 것을 해제해 본다.
+        n += 5;
+    }
+    printf("%d\n", (n == 0) ? three + five : -1); // 5 또는 3으로 나누어 떨어진 경우 합산
+
+    return 0;
+}
