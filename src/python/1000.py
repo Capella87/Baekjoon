e4aa5e484af8357d742b01997cdbfc70e534b1d6
Capella87 <contact@capelladev.com>
2021-02-19 23:44:07 +0900

    Add 2 solutions in Python and 1 solution in C++

---
+++ src/python/1000.py
@@ -0,0 +1,7 @@
+# 백준 2941번 문제 : A+B
+# https://www.acmicpc.net/problem/1000
+# 알고리즘 분류 : 수학, 구현, 사칙연산
+
+a, b = map(int, input().split())
+
+print(a + b)
---
+++ src/python/2748.py
@@ -0,0 +1,16 @@
+# 백준 2941번 문제 : 피보나치 수 2
+# https://www.acmicpc.net/problem/2748
+# 알고리즘 분류 : 수학, 다이나믹 프로그래밍
+
+n = int(input())
+result = f1 = 0
+f2 = 1
+
+for i in range(1, n):
+    result = (f1 + f2)
+    f1 = f2
+    f2 = result
+if n == 1:
+    print(f2)
+else:
+    print(result)
