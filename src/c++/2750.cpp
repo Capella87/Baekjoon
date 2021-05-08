5a8d25db6b395ad2a7257467940736627d17a995
Capella87 <contact@capelladev.com>
2021-05-08 18:36:13 +0900

    Add 2750.cpp in C++

---
+++ src/c++/2750.cpp
@@ -0,0 +1,36 @@
+// 백준 2750번 문제 : 수 정렬하기
+// https://www.acmicpc.net/problem/2750
+// 알고리즘 분류 : 구현, 정렬
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, temp;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> n;
+    int* arr = new int[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - 1 - i; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << '\n';
+
+    delete[] arr;
+    return 0;
+}
