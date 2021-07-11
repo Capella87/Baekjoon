b8f5ae77eccd6b604b543dd0a41f1aef2160fb1f
Capella87 <contact@capelladev.com>
2021-07-11 23:44:29 +0900

    First C# commit

---
+++ src/csharp/9653.cs
@@ -0,0 +1,34 @@
+// 백준 9653번 문제 : 스타워즈 로고
+// https://www.acmicpc.net/problem/9653
+// 알고리즘 분류 : 구현
+
+// Considered as an Easter Egg in this repo.
+
+// 백준 9653번 문제 : 스타워즈 로고
+// https://www.acmicpc.net/problem/9653
+// 알고리즘 분류 : 구현
+
+// Considered as an Easter Egg in this repo.
+
+using System;
+
+namespace starWars
+{
+    class starWars
+    {
+        static void Main(string[] arg)
+        {
+            Console.WriteLine(@"    8888888888  888    88888
+   88     88   88 88   88  88
+    8888  88  88   88  88888
+       88 88 888888888 88   88
+88888888  88 88     88 88    888888
+
+88  88  88   888    88888    888888
+88  88  88  88 88   88  88  88
+88 8888 88 88   88  88888    8888
+ 888  888 888888888 88  88      88
+  88  88  88     88 88   88888888");
+        }
+    }
+}
