6c9d869be8802fd5083c9af9dab6f9f7eeee131a
Capella87 <contact@capelladev.com>
2021-07-17 12:52:07 +0900

    Add notes in 2511.c

--- src/c/2511.c
+++ src/c/2511.c
@@ -20,17 +20,17 @@ int main(void)
     }
     for (i = 0; i < ROUND; i++)
     {
-        if (input[0][i] > input[1][i])
+        if (input[0][i] > input[1][i]) // A wins
         {
             result[i] = 'A';
             atot += 3;
         }
-        else if (input[0][i] < input[1][i])
+        else if (input[0][i] < input[1][i]) // B wins
         {
             result[i] = 'B';
             btot += 3;
         }
-        else if (input[0][i] == input[1][i])
+        else // Draw
         {
             result[i] = 'D';
             atot++;
@@ -44,10 +44,10 @@ int main(void)
         puts("B");
     else if (atot == 10 && btot == 10)
         puts("D");
-    else
+    else // Same scores, but not all draws
     {
         i = ROUND - 1;
-        while (result[i] == 'D' && i >= 0)
+        while (result[i] == 'D' && i >= 0) // Find the last winner before draw
             i--;
         printf("%c\n", result[i]);
     }
