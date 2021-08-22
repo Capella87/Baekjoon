7140457597cfa905d27b6daeeeae454fe87c9083
Capella87 <contact@capelladev.com>
2021-08-22 23:19:55 +0900

    Refine code style of 9095.c

--- src/c/9095.c
+++ src/c/9095.c
@@ -4,25 +4,6 @@
 
 #include <stdio.h>
 // 이 문제는 점화식을 이용하여 재귀 함수로 구현해 풀 수도 있다.
-
-int getCase(int);
-
-int main(void)
-{
-    int n, input;
-
-    scanf("%d", &n);
-    getchar();
-    for (int i = 0; i < n; i++)
-    {
-        scanf("%d", &input);
-        getchar();
-        printf("%d\n", getCase(input));
-    }
-
-    return 0;
-}
-
 // 점화식 A(n) = A(n - 1) + A(n - 2) + A(n - 3)
 
 int getCase(int input)
@@ -30,4 +11,17 @@ int getCase(int input)
     if (input <= 2) return input;
     else if (input == 3) return 4;
     else return getCase(input - 1) + getCase(input - 2) + getCase(input - 3);
+}
+
+int main(void)
+{
+    int n, input;
+
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &input);
+        printf("%d\n", getCase(input));
+    }
+    return 0;
 }
