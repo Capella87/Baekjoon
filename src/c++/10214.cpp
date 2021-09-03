9485df34f748715e4c5b3544508423f839b3330f
Capella87 <contact@capelladev.com>
2021-09-03 20:25:32 +0900

    Rename 1214.cpp to 10214.cpp

--- src/c++/10214.cpp
+++ src/c++/10214.cpp
@@ -1,5 +1,5 @@
-// 백준 1214번 문제 : Baseball
-// https://www.acmicpc.net/problem/1214
+// 백준 10214번 문제 : Baseball
+// https://www.acmicpc.net/problem/10214
 // 알고리즘 분류 : 수학, 구현
 
 #include <iostream>
@@ -8,8 +8,7 @@ using namespace std;
 int main()
 {
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-
+    cin.tie(0); cout.tie(0);
     int t, temp1, temp2;
     int k_rcrd, y_rcrd;
 
@@ -27,6 +26,5 @@ int main()
         else if (k_rcrd < y_rcrd) cout << "Yonsei\n";
         else cout << "Draw\n";
     }
-
     return 0;
 }
