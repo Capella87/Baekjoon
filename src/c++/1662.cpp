91daec6f2187f18c9826bcce8a145987dbc021a0
Capella87 <contact@capelladev.com>
2021-09-26 22:16:01 +0900

    Add 1662.cpp

---
+++ src/c++/1662.cpp
@@ -0,0 +1,56 @@
+// 백준 1662번 문제 : 압축
+// https://www.acmicpc.net/problem/1662
+// 알고리즘 분류 : 자료 구조, 스택, 재귀
+
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+stack <char> brackets;
+
+int getOrigLen(string& str, int begin, int len)
+{
+    int sectionLen = 0;
+    char prev = 0;
+
+    for (int i = begin; i < len; i++)
+    {
+        if (str[i] >= '0' && str[i] <= '9')
+        {
+            sectionLen++;
+            prev = str[i];
+        }
+        else if (str[i] == '(')
+        {
+            --sectionLen;
+            int pStkCount = int(brackets.size());
+            brackets.push('(');
+            sectionLen += int(prev - '0') * getOrigLen(str, i + 1, len);
+            ++i;
+            while ( brackets.size() > pStkCount && i < len)
+            {
+                if (str[i] == ')') brackets.pop();
+                else if (str[i] == '(')
+                    brackets.push('(');
+                i++;
+            }
+            --i;
+        }
+        else if (str[i] == ')') break;
+    }
+    return sectionLen;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    string compressedStr;
+
+    getline(cin, compressedStr);
+    int len = int(compressedStr.length());
+
+    cout << getOrigLen(compressedStr, 0, len) << '\n';
+    return 0;
+}
