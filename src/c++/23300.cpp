9a261974febb23fa381b5d13f70fc623dbf3f325
Capella87 <contact@capelladev.com>
2022-03-15 01:26:38 +0900

    Add 23300.cpp

---
+++ src/c++/23300.cpp
@@ -0,0 +1,85 @@
+// 백준 23300번 문제 : 웹 브라우저 2
+// https://www.acmicpc.net/problem/23300
+// 알고리즘 분류 : 구현, 자료 구조, 덱
+
+#include <iostream>
+#include <deque>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    int n, q, current = 0, target;
+    char op;
+    deque<int> backward, frontward;
+
+    cin >> n >> q;
+    for (int i = 0; i < q; i++)
+    {
+        cin >> op;
+        switch (op)
+        {
+        case 'B':
+            if (backward.size())
+            {
+                frontward.push_front(current);
+                current = backward.front();
+                backward.pop_front();
+            }
+            break;
+
+        case 'F':
+            if (frontward.size())
+            {
+                backward.push_front(current);
+                current = frontward.front();
+                frontward.pop_front();
+            }
+            break;
+
+        case 'A':
+            cin >> target;
+            cin.get();
+            while (!frontward.empty())
+                frontward.pop_front();
+            if (current) backward.push_front(current);
+            current = target;
+            break;
+
+        case 'C':
+            deque<int> temp;
+            while (!backward.empty())
+            {
+                if (temp.empty() || temp.front() != backward.back())
+                    temp.push_front(backward.back());
+                backward.pop_back();
+            }
+            backward = temp;
+            break;
+        }
+    }
+
+    cout << current << '\n';
+    if (backward.empty()) cout << "-1\n";
+    else
+    {
+        while (!backward.empty())
+        {
+            cout << backward.front() << ' ';
+            backward.pop_front();
+        }
+        cout << '\n';
+    }
+    if (frontward.empty()) cout << "-1\n";
+    else
+    {
+        while (!frontward.empty())
+        {
+            cout << frontward.front() << ' ';
+            frontward.pop_front();
+        }
+        cout << '\n';
+    }
+    return 0;
+}
