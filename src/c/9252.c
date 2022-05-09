2af348ae888fcddc3e1ffe143895ec2a848adcad
Capella87 <contact@capelladev.com>
2022-05-09 23:19:54 +0900

    Add 9252.c

---
+++ src/c/9252.c
@@ -0,0 +1,51 @@
+// 백준 9252번 문제 : LCS 2
+// https://www.acmicpc.net/problem/9252
+// 알고리즘 분류 : 다이나믹 프로그래밍
+
+#include <stdio.h>
+#include <string.h>
+#define MAX 1000
+
+char a[MAX + 1];
+char b[MAX + 1];
+int cache[MAX + 1][MAX + 1] = { 0, };
+char lcs[MAX + 1];  
+
+int main(void)
+{
+    scanf("%[^\n]", &a);
+    getchar();
+    scanf("%[^\n]", &b);
+    
+    // Bottom-up
+    int a_size = (int)strlen(a);
+    int b_size = (int)strlen(b);
+    for (int i = 1; i <= a_size; i++)
+    {
+        for (int j = 1; j <= b_size; j++)
+        {
+            if (a[i - 1] == b[j - 1]) // Same character -> Add count
+                cache[i][j] = cache[i - 1][j - 1] + 1;
+            else cache[i][j] = cache[i - 1][j] > cache[i][j - 1] ? cache[i - 1][j] : cache[i][j - 1];
+        }
+    }
+    printf("%d\n", cache[a_size][b_size]);
+
+    int a_idx = a_size, b_idx = b_size;
+    lcs[cache[a_size][b_size]] = '\0';
+    while (a_idx > 0 && b_idx > 0)
+    {
+        if (a[a_idx - 1] == b[b_idx - 1])
+        {
+            lcs[cache[a_idx][b_idx] - 1] = a[a_idx - 1];
+            a_idx--;
+            b_idx--;
+        }
+        else if (cache[a_idx - 1][b_idx] < cache[a_idx][b_idx - 1])
+            b_idx--;
+        else a_idx--;
+    }
+    puts(lcs);
+
+    return 0;
+}
