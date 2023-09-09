9ba8848c306ceea926c1f8acb1f051eba5023192
Capella87 <contact@capelladev.com>
2023-09-09 23:56:46 +0900

    Add 13909.cs

---
+++ src/csharp/13909.cs
@@ -0,0 +1,7 @@
+// 백준 13909번 문제 : 창문 닫기
+// https://www.acmicpc.net/problem/13909
+// 알고리즘 분류 : 수학, 정수론
+
+// Root Square is the answer
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine((int)Math.Pow(n, 0.5));
