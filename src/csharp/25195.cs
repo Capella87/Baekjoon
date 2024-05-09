8de94f3ab18253ec071378e08217b777622d93b6
Capella87 <capella87@outlook.com>
2024-05-09 20:35:38 +0900

    Update 25195.cs to shorter solution

--- src/csharp/25195.cs
+++ src/csharp/25195.cs
@@ -2,64 +2,57 @@
 // https://www.acmicpc.net/problem/25195
 // 알고리즘 분류 : 그래프 이론, 그래프 탐색, 너비 우선 탐색, 깊이 우선 탐색
 
-using System;
-using System.Collections.Generic;
+// conditions[0] for n (the number of vertices), conditions[1] for m (the number of edges)
+var conditions = Array.ConvertAll<string, int>(Console.ReadLine().Split(), int.Parse);
 
-namespace YesOrYes
+var nodes = new List<int>[conditions[0] + 1];
+var isVisited = new bool[conditions[0] + 1];
+var fanInfo = new bool[conditions[0] + 1];
+
+for (int i = 1; i <= conditions[0]; i++)
 {
-    public static class Program
+    nodes[i] = new List<int>();
+}
+
+for (int i = 0; i < conditions[1]; i++)
+{
+    var n = Array.ConvertAll<string, int>(Console.ReadLine().Split(), int.Parse);
+    nodes[n[0]].Add(n[1]);
+}
+var fanCount = int.Parse(Console.ReadLine());
+var fans = Array.ConvertAll<string, int>(Console.ReadLine().Split(), int.Parse);
+for (int i = 0; i < fans.Length; i++)
+{
+    fanInfo[fans[i]] = true;
+}
+
+if (fanInfo[1])
+{
+    Console.WriteLine("Yes");
+    return;
+}
+
+bool hasToMeetGom = false;
+var q = new Queue<int>();
+q.Enqueue(1);
+while (q.Count > 0)
+{
+    int vertex = q.Dequeue();
+    isVisited[vertex] = true;
+
+    if (nodes[vertex].Count == 0)
     {
-        public static int[] _info;
-        public static List<int>[] _nodes;
-        public static bool[] _isFan;
-        public static bool[] _isVisited;
-        public static void Main()
-        {
-            _info = Array.ConvertAll<string, int>(Console.ReadLine().Split(' '), int.Parse);
-            _nodes = new List<int>[_info[0] + 1];
-            for (int i = 1; i <= _info[0]; i++)
-                _nodes[i] = new List<int>();
-            _isFan = new bool[_info[0] + 1];
-            for (int i = 0; i < _info[1]; i++)
-            {
-                var node = Array.ConvertAll<string, int>(Console.ReadLine().Split(' '), int.Parse);
-                _nodes[node[0]].Add(node[1]);
-            }
-            int s = int.Parse(Console.ReadLine());
-            var goms = Array.ConvertAll<string, int>(Console.ReadLine().Split(' '), int.Parse);
-            foreach (var a in goms)
-                _isFan[a] = true;
+        hasToMeetGom = false;
+        break;
+    }
 
-            bool mustGom = false;
-            _isVisited = new bool[_info[0] + 1];
-
-            if (_isFan[1])
-            {
-                Console.WriteLine("Yes");
-                return;
-            }
-            var q = new Queue<int>();
-            q.Enqueue(1);
-            while (q.Count > 0)
-            {
-                var cur = q.Dequeue();
-                _isVisited[cur] = true;
-                if (_nodes[cur].Count == 0)
-                {
-                    mustGom = false;
-                    break;
-                }
-
-                foreach (var d in _nodes[cur])
-                {
-                    if (!_isFan[d] && !_isVisited[d])
-                        q.Enqueue(d);
-                    else if (_isFan[d] && !_isVisited[d])
-                        mustGom = true;
-                }
-            }
-
-            Console.WriteLine(!mustGom ? "yes" : "Yes");
-        }
+    foreach (var v in nodes[vertex])
+    {
+        if (!fanInfo[v] && !isVisited[v])
+            q.Enqueue(v);
+        else if (fanInfo[v] && !isVisited[v])
+            hasToMeetGom = true;
     }
 }
+
+Console.WriteLine(hasToMeetGom ? "Yes" : "yes");
